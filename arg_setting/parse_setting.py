b63c452e919414969de78f20e46fe75a34824c8e
YS-shuai <2504732339@qq.com>
2026-03-08 22:42:16 +0800

    Add files via upload

---
+++ arg_setting/parse_setting.py
@@ -0,0 +1,7 @@
+from arg_setting.parse import Spatio_temporal_setting_s
+
+Parameter_setting = {
+    "moving_mnist": Spatio_temporal_setting_s,
+    "taxibj": Spatio_temporal_setting_s,
+    "weather": Spatio_temporal_setting_s
+}

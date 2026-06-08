c0bdc789cb33e9f69eb161847c64246cc1a4f2c0
YS-shuai <2504732339@qq.com>
2026-06-08 13:38:43 +0800

    Add files via upload

--- models/selection_model.py
+++ models/selection_model.py
@@ -1,5 +1,20 @@
 from models.Mambaformer.model import Mambaformer
+from models.Mambaformer_S.model import Mambaformer_S
+from models.Mambaformer_T.model import Mambaformer_T
+from models.Mambaformer_ST.model import Mambaformer_ST
+from models.Mambaformer_STS.model import Mambaformer_STS
+from models.Mambaformer_TSS.model import Mambaformer_TSS
+from models.Mambaformer_TST.model import Mambaformer_TST
+from models.DynamicNet_multi.model import DynamicNet_multi
+from models.RNN.ct_rnn import CT_RNN
 MODELS = {
     "Mambaformer": Mambaformer,
-
+    "Mambaformer_S": Mambaformer_S,
+    "Mambaformer_T": Mambaformer_T,
+    "Mambaformer_ST": Mambaformer_ST,
+    "Mambaformer_STS": Mambaformer_STS,
+    "Mambaformer_TSS": Mambaformer_TSS,
+    "Mambaformer_TST": Mambaformer_TST,
+    "DynamicNet_multi": DynamicNet_multi,
+    "CT_RNN": CT_RNN,
 }
